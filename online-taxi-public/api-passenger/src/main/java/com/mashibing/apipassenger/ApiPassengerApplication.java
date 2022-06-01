0d89cd81bd1e032c4993a9510f6bcf6e55347fa4
cpf <cpf@msb.com>
2022-06-01 20:26:46 +0800

    api-passenger项目骨架搭建

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -1,2 +1,12 @@
-package com.mashibing.apipassenger;public class ApiPassengerApplication {
+package com.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
 }
--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -1,2 +1,14 @@
-package com.mashibing.apipassenger.controller;public class TestController {
+package com.mashibing.apipassenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+
+        return "test api passenger";
+    }
 }
