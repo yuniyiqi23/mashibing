c704f36c5c2cba90349c565adc44d4ea37f01c31
cpf <cpf@msb.com>
2022-06-13 22:11:48 +0800

    创建 service-passenger-user 服务

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,10 +1,12 @@
 package com.mashibing.apipassenger.service;
 
 import com.mashibing.apipassenger.remote.ServiceVefificationcodeClient;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.NumberCodeResponse;
 import com.mashibing.internalcommon.responese.TokenResponse;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -30,13 +32,22 @@ public class VerificationCodeService {
 
         // 存入redis
         // key,value,过期时间
-        String key = verificationCodePrefix + passengerPhone;
+        String key = generatorKeyByPhone(passengerPhone);
         // 存入redis
         stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
 
         // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
         return ResponseResult.success("");
 
+    }
+
+    /**
+     * 根据乘客手机号，生成验证码的redis key。
+     * @param passengerPhone
+     * @return
+     */
+    private String generatorKeyByPhone(String passengerPhone){
+        return verificationCodePrefix + passengerPhone;
     }
 
     /**
@@ -48,8 +59,21 @@ public class VerificationCodeService {
     public ResponseResult checkCode(String passengerPhone , String verificationCode){
         System.out.println("进入 checkCode 方法");
         // 去redis 读取验证码
+        // 生成key
+        String key = generatorKeyByPhone(passengerPhone);
+        // 根据key去获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的验证码："+codeRedis);
 
         // 校验验证码
+        if (StringUtils.isBlank(codeRedis)){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        if(!verificationCode.trim().equals(codeRedis.trim())){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        System.out.println("验证码校验通过");
+
 
         // 判断原来是否有用户
 
--- online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
+++ online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
@@ -7,6 +7,12 @@ import lombok.Getter;
 public enum CommonStatusEnum {
 
     /**
+     * 短信验证码的错误范围 1000-1099
+     */
+
+    VERIFICATION_CODE_ERROR(1099,"验证码错误"),
+
+    /**
      * 成功
      */
     SUCCESS(1,"success"),
