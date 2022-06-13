07a9678a550c0aaf1c76c7cab16d72467c014936
cpf <cpf@msb.com>
2022-06-13 16:11:00 +0800

    乘客api，发送验证码服务完成。

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
@@ -2,6 +2,7 @@ package com.mashibing.apipassenger.controller;
 
 import com.mashibing.apipassenger.request.VerificationCodeDTO;
 import com.mashibing.apipassenger.service.VerificationCodeService;
+import com.mashibing.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +15,9 @@ public class VerificationCodeController {
     private VerificationCodeService verificationCodeService;
 
     @GetMapping("/verification-code")
-    public String verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+    public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
 
         String passengerPhone = verificationCodeDTO.getPassengerPhone();
-        System.out.println("接受到的手机号参数："+passengerPhone);
         return verificationCodeService.generatorCode(passengerPhone);
 
     }
--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -5,7 +5,10 @@ import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.NumberCodeResponse;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
@@ -13,23 +16,27 @@ public class VerificationCodeService {
     @Autowired
     private ServiceVefificationcodeClient serviceVefificationcodeClient;
 
-    public String generatorCode(String passengerPhone){
-        // 调用验证码服务，获取验证码
-        System.out.println("调用验证码服务，获取验证码");
+    // 乘客验证码的前缀
+    private String verificationCodePrefix = "passenger-verification-code-";
 
-        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(5);
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public ResponseResult generatorCode(String passengerPhone){
+        // 调用验证码服务，获取验证码
+        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
-        System.out.println("remote number code:"+numberCode);
-
         // 存入redis
-        System.out.println("存入redis");
+        // key,value,过期时间
+        String key = verificationCodePrefix + passengerPhone;
+        // 存入redis
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
 
-        // 返回值
-        JSONObject result = new JSONObject();
-        result.put("code",1);
-        result.put("message","success");
-        return result.toString();
+        // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
+
+
+        return ResponseResult.success("");
 
     }
 }
