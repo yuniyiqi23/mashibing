ddbf3e5efb79633db9c54ec0052eebff65f842a7
cpf <cpf@msb.com>
2022-06-15 14:25:00 +0800

    根据手机号查询用户信息

--- online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -3,16 +3,30 @@ package com.mashibing.servicepassengeruser.service;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.VerificationCodeDTO;
+import com.mashibing.servicepassengeruser.dto.PassengerUser;
+import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class UserService {
+
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
 
     public ResponseResult loginOrRegister(String passengerPhone){
         System.out.println("user service被调用，手机号："+passengerPhone);
 
         // 根据手机号查询用户信息
+        Map<String,Object> map = new HashMap<>();
+        map.put("passenger_phone",passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.size()==0?"无记录":passengerUsers.get(0).getPassengerName());
 
         //判断用户信息是否存在
 
