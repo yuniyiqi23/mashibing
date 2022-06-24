d1a1fd9b772bb5d635fa9899e3213820fa69cff4
cpf <cpf@msb.com>
2022-06-24 21:18:58 +0800

    预估价格编写完成

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/service/ForecastPriceService.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/service/ForecastPriceService.java
@@ -1,14 +1,20 @@
 package com.mashibing.apipassenger.service;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.mashibing.apipassenger.remote.ServicePriceClient;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.responese.ForecastPriceResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ForecastPriceService {
+
+    @Autowired
+    ServicePriceClient servicePriceClient;
 
     /**
      * 根据 出发地和目的地经纬度 计算预估价格
@@ -26,9 +32,16 @@ public class ForecastPriceService {
         log.info("目的地纬度："+destLatitude);
 
         log.info("调用计价服务，计算价格");
+        ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
+        forecastPriceDTO.setDepLongitude(depLongitude);
+        forecastPriceDTO.setDepLatitude(depLatitude);
+        forecastPriceDTO.setDestLongitude(destLongitude);
+        forecastPriceDTO.setDestLatitude(destLatitude);
+        ResponseResult<ForecastPriceResponse> forecast = servicePriceClient.forecast(forecastPriceDTO);
+        double price = forecast.getData().getPrice();
 
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
-        forecastPriceResponse.setPrice(12.34);
+        forecastPriceResponse.setPrice(price);
         return ResponseResult.success(forecastPriceResponse);
     }
 }
