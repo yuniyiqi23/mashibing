1f5a0791b0f629031a355d8ffac518055d75bd3d
cpf <cpf@msb.com>
2022-06-30 20:45:42 +0800

    api-boss服务的创建&注册到nacos

---
+++ online-taxi-public/api-boss/src/main/java/com/mashibing/apiBoss/ApiBossApplication.java
@@ -0,0 +1,12 @@
+package com.mashibing.apiBoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ApiBossApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBossApplication.class);
+    }
+}
